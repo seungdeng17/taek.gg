a9692d0d41595f78d260b066913dfee070f98ff7
sgdg <seungdeng17@gmail.com>
2020-06-18 20:14:40 +0900

    [#2] feat: global style, theme 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+import GlobalStyles from '@styles/GlobalStyles';
+
+import MainSearch from '@components/mainSearch/MainSearch';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider {...{ theme }}>
+      <Router>
+        <GlobalStyles />
+        <Switch>
+          <Route exact path='/' component={MainSearch} />
+        </Switch>
+      </Router>
 
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+const theme = {
+    fontSize: {
+        large: '20px',
+        medium: '15px',
+        small: '11px',
+    },
+    color: {
+        brandColor: '#5383e8',
+        nomalFont: '#555e5e',
+        lowFont: '#879292',
+        victory: {
+            font: '#1a78ae',
+            backgroundColor: '#a3cfec',
+            borderColor: '#99b9cf',
+        },
+        defeat: {
+            font: '#c6443e',
+            backgroundColor: '#e2b6b3',
+            borderColor: '#cea7a7',
+        },
+        killBadge: '#ee5a52',
+    },
+}
+
+export default theme;
