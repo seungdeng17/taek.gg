31328e71586432f21fbfa1b1ced9ec5958d48bc1
sgdg <seungdeng17@gmail.com>
2020-07-02 15:25:22 +0900

    [#12] feat: 스펠, 챔피언 데이터 추가 -> 각 아이디 값으로 스펠, 챔피언명 리턴하는 유틸 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,6 @@
+import championsData from '@data/champions.json';
+import spellsData from '@data/spells.json';
+
 export const checkResponseData = response => response.ok && (response.status >= 200 && response.status <= 207);
 
 export const romanNumberConverter = (romanNumber) => {
@@ -8,4 +11,16 @@ export const romanNumberConverter = (romanNumber) => {
         'IV': 4
     }
     return romanNumbers[romanNumber];
+};
+
+export const getChampionName = (championId) => {
+    for (const [championName, value] of Object.entries(championsData.data)) {
+        if (+value.key === championId) return championName;
+    }
+};
+
+export const getSpellName = (spellId) => {
+    for (const [spellName, value] of Object.entries(spellsData.data)) {
+        if (+value.key === spellId) return spellName;
+    }
 };
