8995e071dcb9641b389eee05d7caa3cb706064c1
sgdg <seungdeng17@gmail.com>
2020-07-02 18:07:36 +0900

    [#12] feat: MatchItem 컴포넌트 추가

---
+++ src/components/summonerInfo/match/MatchItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MatchItem = ({ matchInfo }) => {
+  console.log(matchInfo);
+  return <div></div>;
+};
+
+export default MatchItem;
--- src/components/summonerInfo/match/MatchList.jsx
+++ src/components/summonerInfo/match/MatchList.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { getMatchListData, getMatchDatas } from "@modules/match";
+
+import MatchItem from "./MatchItem";
+
+const MatchListWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.color.lowBackground};
+  padding: 15px;
+`;
 
 const MatchList = ({ summonerInfoData }) => {
   const dispatch = useDispatch();
   const { bValidSummoner } = useSelector(({ summoner }) => summoner);
-  const { matchData, index } = useSelector(({ match }) => match);
+  const { matchData, matchesInfo, index } = useSelector(({ match }) => match);
+
+  const matchList = matchesInfo.map((matchInfo) => <MatchItem key={matchInfo.gameId} {...{ matchInfo }} />);
 
   const handleMoreMatches = () => {
     dispatch(getMatchDatas(index.start, index.end, matchData));
@@ -15,7 +27,7 @@ const MatchList = ({ summonerInfoData }) => {
     bValidSummoner && dispatch(getMatchListData(summonerInfoData.accountId));
   }, [dispatch, bValidSummoner, summonerInfoData.accountId]);
 
-  return <div></div>;
+  return <>{matchData && <MatchListWrap>{matchList}</MatchListWrap>}</>;
 };
 
 export default MatchList;
